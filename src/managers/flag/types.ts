c7330aa9905a8b330d3465a1f22248108076d98f
Mike Chabot <mike.chabot@live.com>
2025-06-20 14:36:06 -0400

    Add fallback type

--- src/managers/flag/types.ts
+++ src/managers/flag/types.ts
@@ -1,7 +1,13 @@
 import { SdkUserContext } from '~api/data-contracts';
 
 export interface FeatureFlags {}
-export type FlagKey = keyof FeatureFlags;
+
+/**
+ * Add a fallback in the event the user has not run the CLI type generator
+ */
+export type FlagKey = [keyof FeatureFlags] extends [never]
+  ? string
+  : keyof FeatureFlags;
 
 export interface IFlagManager {
   flag: <T>(context: SdkUserContext, flagKey: FlagKey, defaultValue?: T) => T;
